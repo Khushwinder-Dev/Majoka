63ff9a25cee73e48d03a4a22229059e2ca999c1f
Khushwinder <154021262+Khushwinder-Dev@users.noreply.github.com>
2026-09-03 23:12:46 +0530

    Git pages fixes

--- next.config.ts
+++ next.config.ts
@@ -1,16 +1,10 @@
 import type { NextConfig } from "next";
 
-const isProd = process.env.NODE_ENV === "production";
-
 const nextConfig: NextConfig = {
   output: "export",
-  basePath: isProd ? "/Majoka" : "",
-  assetPrefix: isProd ? "/Majoka/" : "",
   trailingSlash: true,
   images: {
     unoptimized: true,
-    loader: "custom",
-    loaderFile: "./imageLoader.ts",
     remotePatterns: [
       {
         protocol: "https",
